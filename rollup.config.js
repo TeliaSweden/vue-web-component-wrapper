e925c71d1625602265d2ff9b0f3a02847d001117
William Bernting <william.bernting@kits.se>
2020-06-11 09:23:51 +0200

    vue-wc-wrapper.es5.js -> vue-wc-wrapper.umd.js + esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,43 @@
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 export default [
   {
     input: 'src/index.js',
     output: [
       {
-        format: 'es',
-        file: 'dist/vue-wc-wrapper.js'
-      },
-      {
-        format: 'iife',
-        name: 'wrapVueWebComponent',
-        file: 'dist/vue-wc-wrapper.global.js'
+        exports: 'named',
+        format: 'esm',
+        dir: 'dist/esm',
+        sourcemap: true
       }
-    ]
+    ],
+    preserveModules: true
   },
   {
     input: 'src/index.js',
     output: [
       {
-        format: 'es',
-        file: 'dist/vue-wc-wrapper.es5.js'
+        exports: 'named',
+        name: 'wrap',
+        format: 'umd',
+        file: 'dist/vue-wc-wrapper.umd.js',
+        sourcemap: true
       },
       {
         format: 'iife',
         name: 'wrapVueWebComponent',
-        file: 'dist/vue-wc-wrapper.es5.global.js'
+        file: 'dist/vue-wc-wrapper.global.js',
+        sourcemap: true
       }
     ],
-    plugins: [babel()]
+    plugins: [
+      babel(),
+      terser({
+        output: {
+          ecma: 5
+        }
+      })
+    ]
   }
 ]
